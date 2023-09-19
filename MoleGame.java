177fd481f4f7062034c379e97253695dc1129531
kiwoom <dk960227@hanmail.net>
2023-09-19 19:43:07 +0900

    20230919_20203354_choi

--- MoleGame.java
+++ MoleGame.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MoleGame extends JFrame {
     private JButton[] buttons;
@@ -7,12 +10,21 @@ public class MoleGame extends JFrame {
     public MoleGame(){
         setTitle("MOLE_GAME_JAVA");
         setSize(1920,1080);
-        buttons = new JButton[5];
-        for (int i =0; i < buttons.size; i++){
-            buttons[i] = new JButton("MOLE");
+        setLayout(new GridLayout(3, 3));
+        buttons = new JButton[9];
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton("Mole");
+            buttons[i].setFont(new Font("Arial", Font.PLAIN, 20));
+            buttons[i].setEnabled(false);
+            buttons[i].addActionListener(new MoleListener());
             add(buttons[i]);
         }
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+    }
+    private class MoleListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        }
     }
 }
